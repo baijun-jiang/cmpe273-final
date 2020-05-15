c8118407fc15acefdb007f0d74f7262faa480c3b
Baijun Jiang <baijunjiang@Baijuns-MacBook-Air.local>
2020-05-14 21:43:23 -0700

    final

--- final/merkletree/merkle_trees.py
+++ final/merkletree/merkle_trees.py
@@ -1,5 +1,6 @@
 import hashlib
 from dataclasses import dataclass
+from hash_files import read_files_and_hash
 
 class Node(object):
     def __init__(self, val=None, left=None, right=None):
@@ -19,6 +20,7 @@ class MerkleTrees(object):
         self.root = None
         # txns dict: { hash_val -> 'file_path' } 
         self.txns = None
+        self.nodes = []
         
     def get_root_hash(self):
         return self.root.val if self.root else None
@@ -33,13 +35,39 @@ class MerkleTrees(object):
         if len(txns_list)%2 != 0:
             txns_list.append(txns_list[-1])
         
+        nodes = []
+
         for index in range(0, len(txns_list)-1, 2):
             left = txns_list[index]
             right = txns_list[index+1]
             combine = left + right
             root = hashlib.sha256(combine.encode()).hexdigest()
             current_node = Node(root, Node(left), Node(right))
-            # TODO
+            self.nodes.append(current_node)
+            nodes.append(current_node)
+        
+        if len(nodes) >= 2:
+            roots = self.build_nodes(nodes)
+            self.root = roots[0]
+            return roots[0]
+    
+    def build_nodes(self, nodes):
+
+        if len(nodes) == 1:
+            return nodes
+
+        results = []
+
+        for index in range(0, len(nodes) - 1, 2):
+            left = nodes[index]
+            right = nodes[index+1]
+            combine = left.val + right.val
+            root = hashlib.sha256(combine.encode()).hexdigest()
+            current_node = Node(root, Node(left.val), Node(right.val))
+            self.nodes.append(current_node)
+            results.append(current_node)
+        
+        return self.build_nodes(results)
 
 
     def print_level_order(self):
@@ -48,7 +76,32 @@ class MerkleTrees(object):
          / \     -> --------------------    
         2   3       2 3
         """
-        # TODO
+        height = self.get_tree_height(self.root)
+        for i in range(1, height + 1):
+            self.print_val_at_level(self.root, i)
+    
+
+    def print_val_at_level(self, root, level):
+        if root == None:
+            return 
+        
+        if level == 1:
+            print(root.val)
+        
+        if level > 1:
+            self.print_val_at_level(root.left, level - 1)
+            self.print_val_at_level(root.right, level - 1)
+    
+
+    def get_tree_height(self, node):
+        
+        if node == None:
+            return 0
+        
+        l_height = self.get_tree_height(node.left)
+        r_height = self.get_tree_height(node.right)
+
+        return max(l_height, r_height) + 1
         
 
     @staticmethod
@@ -64,8 +117,37 @@ class MerkleTrees(object):
         """
         diff = []
         if x.get_root_hash() == y.get_root_hash():
-            return diff
+            diff_left = MerkleTrees.compare_nodes(x.root, y.root)
+            diff_right = MerkleTrees.compare_nodes(x.root, y.root)
+
+            return diff_left + diff_right
         
-        # TODO
+        elif x.get_root_hash() != y.get_root_hash():
+            diff.append((x.get_root_hash(), y.get_root_hash()))
+
+            diff_left = MerkleTrees.compare_nodes(x.root, y.root)
+            diff_right = MerkleTrees.compare_nodes(x.root, y.root)
+
+            return diff + diff_left + diff_right
+        
         
         return diff
+
+    @staticmethod
+    def compare_nodes(x, y):
+
+        if x == None:
+            return []
+
+        diff = [] 
+
+        if x.val != y.val:
+            diff.append((x.val, y.val))
+            
+        diff_left = MerkleTrees.compare_nodes(x.left, y.left)
+        diff_right = MerkleTrees.compare_nodes(x.right, y.right)
+
+        if len(diff) > 0:
+            return diff_left + diff + diff_right
+        
+        return diff_left + diff_right
